1a12a1b07fc4c23decee4708af5b6654b6a028cb
Tony Robinson <tonyrobinson@31.247.21.198.tigernet.wifi.dyn.clemson.edu>
2016-04-06 12:50:12 -0400

    this should store all the student info in the student oobjects

--- CpscCourseMain.cpp
+++ CpscCourseMain.cpp
@@ -1,3 +1,10 @@
+#include "Student.h"
+#include "CpscCourse.h"
+#include <vector>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
 /*Prototypes */
 void fillStudents(ifstream&, vector<Student>&);
 void fillCpscCourse(ifstream&, vector<CpscCourse>&);
@@ -6,6 +13,8 @@ void fillCpscCourse(ifstream&, vector<CpscCourse>&);
 int main() {
     /*Suggestion: create two vectors to hold the information pertaining to 
       CpscCourse and Student*/
+      vector <Student> studentInfo;
+      vector <CpscCourse> courseInfo;
 	
 	
 	/*Call fillStudents and fill CpscCourse */
@@ -15,11 +24,41 @@ int main() {
 }
 
 
-void fillStudents(ifstream& inStudent, vector<Student>& stu) {
+void fillStudents(ifstream& inStudent, vector<Student>& stu) 
+{
+  string stuLast, stuFirst, classStanding;
+  int term, creditHours;
+  float GPA;
+  
+ inStudent.open("studentInfo.txt");
+ if(inStudent.is_open())
+ {
+
+  inStudent >> term;
+  
+  for(int x= 0; x < term; x++)
+  {
+   stu.push_back(newStudent);
+
+   stu[x].setLastName(getline(inStudent,stuLast)));
+   stu[x].setFirstName(getline(inStudent,stuFirst)));
+   inStudent << GPA;
+   stu[x].setGPA(GPA);
+   stu[x].setClassStanding(getline(inStudent,classStanding)));
+   inStudent >> creditHours;
+   stu[x].setCurCredit(creditHours);
+   inStudent.ingore(1,'\n');
+   
+  }
+
+
+ }
+
 
 }
 
 
-void fillCpscCourse(ifstream& in, vector <CpscCourse>& course) {
+void fillCpscCourse(ifstream& in, vector <CpscCourse>& course) 
+{
 
 }
