bfafa881fadf742060a4663b18309e432a113525
Tony Robinson <tonyrobinson@31.247.21.198.tigernet.wifi.dyn.clemson.edu>
2016-04-06 11:36:34 -0400

    /*I added the stuff for Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,32 +1,38 @@
+#include <iostream>
+#include "Person.h"
+using namespace std;
+
 Person()
 {
-
+   firstName = " ";
+   lastName = " ";
 }
-Person(string, string)
+Person(string first, string last)
 {
-
+   firstName = first;
+   lastName = last;
 }
 ~Person()
 {
-
+   cout << "Person object is being destructed!!!" << endl;
 }
 void setFirstName(string firstn)
 {
-
+   firstName = firstn;
 }
 void setLastName(string lastn)
 {
-
+   lastName = lastn;
 }
 string getFirstName()
 {
-
+   return firstName;
 }
 string getLastName()
 {
-
+   return lastName;
 }
 void printPInfo()
 {
-	
+   cout << firstName << "," << lastName 
 }
